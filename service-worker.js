09b29752e0055335a63b846d9191d61320ba3104
Jérôme Legrand <jerome.legrand4537@gmail.com>
2018-04-18 21:02:58 +0200

    nettoyage app.js et renommage du cache service worker

--- service-worker.js
+++ service-worker.js
@@ -12,26 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var dataCacheName = 'weatherData-v1';
-var cacheName = 'weatherPWA-final-1';
-var filesToCache = [
+const dataCacheName = 'projetIoData-v1';
+const cacheName = 'projetIo';
+const filesToCache = [
   '/',
   '/index.html',
   '/scripts/app.js',
   '/styles/inline.css',
-  '/images/clear.png',
-  '/images/cloudy-scattered-showers.png',
-  '/images/cloudy.png',
-  '/images/fog.png',
-  '/images/ic_add_white_24px.svg',
-  '/images/ic_refresh_white_24px.svg',
-  '/images/partly-cloudy.png',
-  '/images/rain.png',
-  '/images/scattered-showers.png',
-  '/images/sleet.png',
-  '/images/snow.png',
-  '/images/thunderstorm.png',
-  '/images/wind.png'
 ];
 
 self.addEventListener('install', function(e) {
