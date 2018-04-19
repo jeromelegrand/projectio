509d1d8afc86743b2b019240a907a159dd40cda5
Jérôme Legrand <jerome.legrand4537@gmail.com>
2018-04-19 19:33:19 +0200

    correction du cache

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,7 @@ const filesToCache = [
     'https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css',
     'https://use.fontawesome.com/releases/v5.0.10/js/all.js',
     '/node_modules/adler32cs/adler32cs.js',
-    '/node_modules/cd-blob.js/Blob.js',
+    '/node_modules/cf-blob.js/Blob.js',
     '/node_modules/file-saver/FileSaver.js',
     'https://unpkg.com/jspdf@latest/dist/jspdf.min.js',
 ];
